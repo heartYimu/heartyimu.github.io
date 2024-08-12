5900416ce8d3a022db1befe44f3b8665d3b8ed20
Yimu <heartanal@163.com>
2024-08-13 00:08:59 +0800

    Site updated: 2024-08-13 00:08:59

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1723478427555";
+const VERSION = "1723478935379";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
